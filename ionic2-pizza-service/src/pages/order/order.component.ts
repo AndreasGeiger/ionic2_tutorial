b1932e4cb4f376e2603e06a9661ccd88cc49f937
Andreas Geiger <geiger911@gmail.com>
2016-11-04 21:03:47 +0100

    Add scrollToTop() and scrollToButtom() methods with the specific buttons

--- ionic2-pizza-service/src/pages/order/order.component.ts
+++ ionic2-pizza-service/src/pages/order/order.component.ts
@@ -1,9 +1,24 @@
-import {Component} from '@angular/core'
+import {Component, ViewChild} from '@angular/core';
+import {Content} from 'ionic-angular';
 
 @Component({
-  template: '<ion-content><strong>Hello World</strong></ion-content>'
+  template: `<ion-content>
+    <button ion-button (click)="scrollToBottom()">To Bottom</button>
+    <div style="height: 2000px;">
+      <b>Hello</b> World!
+    </div>
+    <b>on bottom</b>
+    <button ion-button (click)="scrollToTop()">To Top</button>
+  </ion-content>`
 })
 
 export class OrderComponent{
-  
+  @ViewChild(Content) content : Content;
+
+  scrollToTop(){
+    this.content.scrollToTop();
+  }
+  scrollToBottom(){
+    this.content.scrollToBottom();
+  }
 }
