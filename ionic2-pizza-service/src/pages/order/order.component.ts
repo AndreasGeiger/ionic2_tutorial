25d074b3a3cc49c7dd8e68b43ba0c6798353368c
Andreas Geiger <geiger911@gmail.com>
2016-11-04 21:14:17 +0100

    Add header and footer

--- ionic2-pizza-service/src/pages/order/order.component.ts
+++ ionic2-pizza-service/src/pages/order/order.component.ts
@@ -2,14 +2,7 @@ import {Component, ViewChild} from '@angular/core';
 import {Content} from 'ionic-angular';
 
 @Component({
-  template: `<ion-content>
-    <button ion-button (click)="scrollToBottom()">To Bottom</button>
-    <div style="height: 2000px;">
-      <b>Hello</b> World!
-    </div>
-    <b>on bottom</b>
-    <button ion-button (click)="scrollToTop()">To Top</button>
-  </ion-content>`
+  templateUrl: './order.component.html'
 })
 
 export class OrderComponent{
