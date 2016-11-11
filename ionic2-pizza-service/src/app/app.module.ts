76a70969cf6092ec77094e365813df7c5d844761
Andreas Geiger <geiger911@gmail.com>
2016-11-11 10:08:07 +0100

    Implement NavController

--- ionic2-pizza-service/src/app/app.module.ts
+++ ionic2-pizza-service/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { PizzaAppComponent } from './app.component';
 import { OrderComponent } from '../pages/order/order.component';
+import { AboutComponent } from '../about/index';
 
 @NgModule({
   declarations: [
     PizzaAppComponent,
-    OrderComponent
+    OrderComponent,
+    AboutComponent
   ],
   imports: [
     IonicModule.forRoot(PizzaAppComponent)
@@ -14,7 +16,8 @@ import { OrderComponent } from '../pages/order/order.component';
   bootstrap: [IonicApp],
   entryComponents: [
     PizzaAppComponent,
-    OrderComponent
+    OrderComponent,
+    AboutComponent
   ],
   providers: []
 })
--- ionic2-pizza-service/src/pages/order/order.component.ts
+++ ionic2-pizza-service/src/pages/order/order.component.ts
@@ -1,11 +1,20 @@
 import {Component, ViewChild} from '@angular/core';
 import {Content} from 'ionic-angular';
+import {NavController} from 'ionic-angular';
+import {AboutComponent} from '../../about/index';
 
 @Component({
-  templateUrl: './order.component.html'
+  templateUrl: 'order.component.html'
 })
 
 export class OrderComponent{
+  aboutComponent = AboutComponent;
+  constructor (private nav: NavController){
+
+  }
+  openAbout() {
+    this.nav.push(AboutComponent);
+  }
   @ViewChild(Content) content : Content;
 
   scrollToTop(){
